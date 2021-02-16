52e53321f5ca35f9854915f96e96d023c5fe40f4
adsryen <prl1594959462@gmail.com>
2021-02-16 15:47:54 +0800

    优化

--- smtp-auto.py
+++ smtp-auto.py
@@ -1,5 +1,6 @@
 import smtplib
 import datetime
+import time
 from email import encoders
 from email.header import Header
 from email.mime.multipart import MIMEMultipart
@@ -8,6 +9,10 @@ from email.utils import parseaddr, formataddr
 from email.mime.application import MIMEApplication
 
 def send_mail(areceiver):
+    # 设置邮箱服务器地址以及端口
+    smtp_server = "smtp.qq.com"
+    server = smtplib.SMTP(smtp_server, 25)
+    server.set_debuglevel(1)
     # 发件人邮箱
     asender = "blyat@qq.com"
     # 收件人邮箱
@@ -20,7 +25,7 @@ def send_mail(areceiver):
     # 发件人地址
     from_addr = "blyat@qq.com"
     # 邮箱密码（授权码）
-    password = "opnwjnqjrsxmcafe"
+    password = "bewxkelaphsgbjfe"
 
     # 邮件设置
     msg = MIMEMultipart()
@@ -55,36 +60,34 @@ def send_mail(areceiver):
     msg.attach(xlsxpart)
     '''
 
-    # 设置邮箱服务器地址以及端口
-    smtp_server = "smtp.qq.com"
-    server = smtplib.SMTP(smtp_server, 25)
-    server.set_debuglevel(1)
     # 登陆邮箱
     server.login(from_addr, password)
     # 当前时间
     now_time = datetime.datetime.now()
     #去掉输出日志时的邮件首尾自带的换行符
     areceiver_strip = areceiver.strip()
-    # 发送邮件
+    # 发送邮件(日志生成在当前目录)
     try:
         server.sendmail(from_addr,areceiver.split(',') + acc.split(','), msg.as_string())
         print(areceiver + "邮件发送成功")
-        with open(r'C:\Users\Administrator\Desktop\邮件自动化\记录.log', 'a+') as f:
+        with open(r'记录.log', 'a+') as f:
             f.write(areceiver_strip + "-----邮件发送成功------------------" + str(now_time) + '\n' + '\n')
 
     except smtplib.SMTPException:
         print(areceiver + "Error: 无法发送邮件")
-        with open(r'C:\Users\Administrator\Desktop\邮件自动化\记录.log', 'a') as f:
+        with open(r'记录.log', 'a') as f:
             f.write(areceiver_strip + "-----Error: 无法发送邮件------------------" + str(now_time) + '\n' + '\n')
     # 断开服务器链接
     '''server.quit()'''
 
-#多个收件人文件列表
-fileopen = open('C:\\Users\\Administrator\\Desktop\\邮件自动化\\receiver.txt', encoding='utf-8')
+#多个收件人文件列表（文件在同一目录下，不是同一目录需加' \\ ' ）
+fileopen = open('receiver.txt', encoding='utf-8')
 receivers = fileopen.readlines()
 #遍历发送邮件
 for receiver in receivers:
     '''receiver = line.strip('\n') #去掉换行符
     print(receiver)'''
     send_mail(receiver)
+    #qq邮箱限制每日500封，每200秒发送一封
+    time.sleep(200)
 
--- 迭代测试.py
+++ 迭代测试.py
@@ -1,6 +1,8 @@
-fileopen = open('C:\\Users\\Administrator\\Desktop\\邮件自动化\\receiver.txt', encoding='utf-8')
+fileopen = open('receiver.txt', encoding='utf-8')
 receivers = fileopen.readlines()
 
 for receiver in receivers:
     '''receiver = line.strip('\n') #去掉换行符'''
     print(receiver)
+    with open(r'cesi记录.log', 'a+') as f:
+        f.write(receiver + "-----邮件发送成功------------------"  + '\n' + '\n')
