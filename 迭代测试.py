75592621a6828bd46660fee35f8d01c200087cf6
adsryen <prl1594959462@gmail.com>
2021-02-27 03:30:25 +0800

    fix

--- 迭代测试.py
+++ 迭代测试.py
@@ -1,8 +1,13 @@
+#多个收件人文件列表1（文件在同一目录下，不是同一目录需加' \\ ' ）
 fileopen = open('receiver.txt', encoding='utf-8')
 receivers = fileopen.readlines()
 
-for receiver in receivers:
+#多个收件人文件列表2（文件在同一目录下，不是同一目录需加' \\ ' ）
+fileopen_2 = open('receiver2.txt', encoding='utf-8')
+receivers_2 = fileopen_2.readlines()
+
+for receiver,receiver_2 in zip(receivers,receivers_2):
     '''receiver = line.strip('\n') #去掉换行符'''
-    print(receiver)
-    with open(r'cesi记录.log', 'a+') as f:
-        f.write(receiver + "-----邮件发送成功------------------"  + '\n' + '\n')
+    receiver_strip = receiver.strip()
+    receiver_2_strip = receiver_2.strip()
+    print(receiver_strip + '和' + receiver_2_strip)
